368e55fb5047e0a61dee0c65a2983a09394d8ff4
RCornejoM <rodrigo.cornejo.m@ug.uchile.cl>
2025-10-08 18:19:15 -0300

    feat: navigation between pages working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from sqlalchemy import select, desc
 from db import get_session
 
@@ -7,8 +7,22 @@ app = Flask(__name__)
 @app.route('/')
 def portada():
     session = get_session()
-
     return render_template('index.html')
+
+@app.route('/aviso_form', methods=['GET', 'POST'])
+def aviso_form():
+    if request.method == 'POST':
+        return redirect('/portada')
+    return render_template('aviso_form.html')
+
+@app.route('/aviso_list')
+def aviso_list():
+    return render_template('aviso_list.html')
+    
+@app.route('/stats')
+def stats():
+    return render_template('stats.html')
+
 
 if __name__ == "__main__":
     app.run()
